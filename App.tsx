a2a559ed1164cb395932f49b97129082bfb3e27a
Makhare Makharadze <64287262+Makhare01@users.noreply.github.com>
2025-03-05 12:52:45 +0400

    feat: add custom checkbox component (#1)

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { CustomCheckbox } from 'components/checkbox';
 import { ContainerView } from 'components/container-view';
 import { Header } from 'components/header';
 import { StatusBar } from 'expo-status-bar';
@@ -10,6 +11,7 @@ export default function App() {
     <ThemeProvider>
       <ContainerView>
         <Header />
+        <CustomCheckbox label="Label" description="Description" color="secondary" required />
         <StatusBar style="auto" />
       </ContainerView>
     </ThemeProvider>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,17 +7,91 @@ module.exports = {
     extend: {
       colors: {
         // text
-        text_primary: 'var(--text-primary)',
-        text_secondary: 'var(--text-secondary)',
-        text_placeholder: 'var(--text-placeholder)',
-        text_disabled: 'var(--text-disabled)',
+        text: {
+          primary: 'var(--text-primary)',
+          secondary: 'var(--text-secondary)',
+          placeholder: 'var(--text-placeholder)',
+          disabled: 'var(--text-disabled)',
+        },
 
         // foreground
-        foreground_default: 'var(--foreground-default)',
-        foreground_hover: 'var(--foreground-hover)',
-        foreground_press: 'var(--foreground-press)',
-        foreground_selected: 'var(--foreground-selected)',
-        foreground_disabled: 'var(--foreground-disabled)',
+        foreground: {
+          default: 'var(--foreground-default)',
+          hover: 'var(--foreground-hover)',
+          press: 'var(--foreground-press)',
+          selected: 'var(--foreground-selected)',
+          disabled: 'var(--foreground-disabled)',
+        },
+
+        // primary
+        primary: {
+          dark: 'var(--primary-dark)',
+          base: 'var(--primary-base)',
+          light: 'var(--primary-light)',
+          'op-24': 'var(--primary-op-24)',
+          'op-16': 'var(--primary-op-16)',
+          'op-8': 'var(--primary-op-8)',
+        },
+
+        // secondary
+        secondary: {
+          dark: 'var(--secondary-dark)',
+          base: 'var(--secondary-base)',
+          light: 'var(--secondary-light)',
+          'op-24': 'var(--secondary-op-24)',
+          'op-16': 'var(--secondary-op-16)',
+          'op-8': 'var(--secondary-op-8)',
+        },
+
+        // success
+        success: {
+          dark: 'var(--success-dark)',
+          base: 'var(--success-base)',
+          light: 'var(--success-light)',
+          'op-24': 'var(--success-op-24)',
+          'op-16': 'var(--success-op-16)',
+          'op-8': 'var(--success-op-8)',
+        },
+
+        // warning
+        warning: {
+          dark: 'var(--warning-dark)',
+          base: 'var(--warning-base)',
+          light: 'var(--warning-light)',
+          'op-24': 'var(--warning-op-24)',
+          'op-16': 'var(--warning-op-16)',
+          'op-8': 'var(--warning-op-8)',
+        },
+
+        // error
+        error: {
+          dark: 'var(--error-dark)',
+          base: 'var(--error-base)',
+          light: 'var(--error-light)',
+          'op-24': 'var(--error-op-24)',
+          'op-16': 'var(--error-op-16)',
+          'op-8': 'var(--error-op-8)',
+        },
+
+        // informative
+        info: {
+          dark: 'var(--info-dark)',
+          base: 'var(--info-base)',
+          light: 'var(--info-light)',
+          'op-24': 'var(--info-op-24)',
+          'op-16': 'var(--info-op-16)',
+          'op-8': 'var(--info-op-8)',
+        },
+
+        // grayscale
+        gray: {
+          dark: 'var(--gray-dark)',
+          base: 'var(--gray-base)',
+          light: 'var(--gray-light)',
+          'op-24': 'var(--gray-op-24)',
+          'op-16': 'var(--gray-op-16)',
+          'op-8': 'var(--gray-op-8)',
+        },
       },
     },
   },
